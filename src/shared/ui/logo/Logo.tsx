1478e7b8b654eb6fe9dd878d813c8211fbd628e6
Maksim Kulishevich <maxim8992012@gmail.com>
2025-05-29 11:10:53 +0300

    feat: add logo logic

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -1,11 +1,33 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import s from "./Logo.module.scss";
 import clsx from "clsx";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { paths } from "@/shared/config/constants/paths";
 
 export const Logo = ({ className }: { className?: string }) => {
+  const pathname = usePathname();
+
+  if (pathname === "/") {
+    return (
+      <div className={clsx(s.logoContainer, className)}>
+        <div className={s.image}>
+          <Image src={"/logo.png"} fill alt="image" />
+        </div>
+        <div className={s.title}>
+          <div className={s.logo}>
+            <Image src={"/logo_oven.png"} fill alt="logo" />
+          </div>
+          <p className="body_3">официальный дилер</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className={clsx(s.logoContainer, className)}>
+    <Link href={paths.home} className={clsx(s.logoContainer, className)}>
       <div className={s.image}>
         <Image src={"/logo.png"} fill alt="image" />
       </div>
@@ -15,6 +37,6 @@ export const Logo = ({ className }: { className?: string }) => {
         </div>
         <p className="body_3">официальный дилер</p>
       </div>
-    </div>
+    </Link>
   );
 };
